fe952f7c7b51ded1dd0fde54f718b01611290f63
Marcos Fuerte <marcos.fuerte.work@gmail.com>
2024-09-16 20:28:30 -0700

    ThetaTerminal
    https://www.thetadata.net/

---
+++ ThetaTerminal/run.bat
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2818
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .SFNSMono-Light_YAXS140B3BC_wght1F40000;}
+{\colortbl;\red255\green255\blue255;\red29\green29\blue29;\red242\green245\blue249;}
+{\*\expandedcolortbl;;\cssrgb\c14902\c14902\c14902\c90196;\cssrgb\c96078\c96863\c98039\c50196;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 java -jar ThetaTerminal.jar marcos.fuerte.work@gmail.com asdfQWER<>12\
+PAUSE\
+}
